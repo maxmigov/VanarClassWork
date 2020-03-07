9616bb7ba280b3ffb85c9caa50633bdd98740690
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-07 15:57:18 +0200

    Add files via upload

--- 070320/src/main/App.java
+++ 070320/src/main/App.java
@@ -1,12 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 import providers.NasaDataProvider;
 
 public class App {
 
 	public static void main(String[] args) throws Exception {
+	
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter start/end dates ");
 		
-		new NasaDataProvider().getNeoAsteroids();
+		String start = in.next(), end = in.next();
+		
+		new NasaDataProvider().getNeoAsteroids(start, end);
 		
 	}
 
