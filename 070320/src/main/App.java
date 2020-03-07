b81766f6ea3d667c936d00c6982323438368b333
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-07 15:23:37 +0200

    Add files via upload

--- 070320/src/main/App.java
+++ 070320/src/main/App.java
@@ -1,1 +1,13 @@
+package main;
 
+import providers.NasaDataProvider;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		
+		new NasaDataProvider().getNeoAsteroids();
+		
+	}
+
+}
