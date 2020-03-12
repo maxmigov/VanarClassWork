dc238c783b67774e88b2dfb91de9167c44023c50
maxmigov <60349576+maxmigov@users.noreply.github.com>
2020-03-12 04:53:32 +0200

    Try Catch implementation

--- 070320/src/main/App.java
+++ 070320/src/main/App.java
@@ -1,20 +1,32 @@
-package main;
+package src.main;
 
+import java.io.IOException;
 import java.util.Scanner;
 
-import providers.NasaDataProvider;
+import src.providers.Asteroids;
+import src.providers.NasaDataProvider;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
-	
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter start/end dates ");
-		
-		String start = in.next(), end = in.next();
-		
-		new NasaDataProvider().getNeoAsteroids(start, end);
-		
-	}
+    public static void main(String[] args) throws Exception {
+
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Expected format (yyyy-mm-dd) - The Feed date limit is only 7 Days");
+            System.out.println("Enter start/end dates ");
+
+            String start = in.next(), end = in.next();
+
+            new NasaDataProvider().getNeoAsteroids(start, end);
+
+
+
+        }
+        catch (IOException e){
+            System.out.println("400 Bad Request");
+            System.out.println("The Feed date limit is only 7 Days");
+        }
+
+    }
 
 }
